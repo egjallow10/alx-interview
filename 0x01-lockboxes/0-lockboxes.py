04428eb1738f05ca86b4b553ae7007fdbca23459
egjallo <ejallow@insistglobal.com>
2023-03-30 20:32:20 +0000

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Determine if all boxes have key"""
+
+
+def canUnlockAll(boxes):
+    """Return a true or false if key"""
+    lenofBox = len(boxes)
+    visited = [False] * lenofBox  
+    visited[0] = True     
+    queue = [0]           
+    while queue:
+        box = queue.pop(0)
+        for key in boxes[box]:
+            if key < lenofBox and not visited[key]:
+                visited[key] = True
+                queue.append(key)
+    return all(visited)
