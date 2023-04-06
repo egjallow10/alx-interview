2889642401e684bb137dec67bc3ac16268b10c8e
egjallow10 <egjallow10@gmail.com>
2023-04-06 23:34:42 +0000

    miniOperation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+function that calculates the fewest number of 
+operations
+"""
+
+def minOperations(n):
+    """Return the minimal operation"""
+    if n <= 1:
+        return 0
+    
+    factors = []
+    for i in range(2, int(n**0.5) + 1):
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+    if n > 1:
+        factors.append(n)
+    
+    num_ops = sum(factors)
+    
+    if num_ops == 0:
+        return 0
+    
+    return num_ops
