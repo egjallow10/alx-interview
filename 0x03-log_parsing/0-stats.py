66faabf5715f4a8d27b15271431f58022c13815a
egjallow10 <egjallow10@gmail.com>
2023-04-13 23:48:50 +0000

    changes

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """Performs log analysis"""
 
-
 import sys
+
 counter = 0
 file_size = 0
-statusC_counte = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 405: 0, 500: 0}
-stdin = sys.stdin
-
+statusC_counte = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
 try:
-    for line in stdin:
+    while True:
+        line = input().strip()
         output = line.split()
         if int(output[-2]) in statusC_counte.keys() and len(output) == 9:
             counter += 1
@@ -18,10 +17,11 @@ try:
             file_size += int(output[-1])
         if counter % 10 == 0:
             print('File size: {}'.format(file_size))
-            for key, value in statusC_counte.items():
+            for key, value in sorted(statusC_counte.items()):
                 if value:
                     print("{}: {}".format(key, value))
-except KeyboardInterrupt as error:
+except EOFError:
     print('File size: {}'.format(file_size))
-    for key, value in statusC_counte.items():
-        print("{}: {}".format(key, value))
+    for key, value in sorted(statusC_counte.items()):
+        if value:
+            print("{}: {}".format(key, value))
