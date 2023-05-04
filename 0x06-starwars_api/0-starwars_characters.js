3cc5f6413d24559031f6faafdbbe35fc6bebf069
egjallo <egjallo@insistglobal.com>
2023-05-04 16:42:43 +0000

    chage

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,23 @@
 #!/usr/bin/node
 
-const request = require('request');
-const BASE_URL = 'https://swapi-api.alx-tools.com/api';
+const requests = require('request');
+const { json } = require('stream/consumers');
+const BASE_URL = `https://swapi-api.alx-tools.com/api`
+const argument = process.argv[2]
 
-const argument = process.argv[2];
+requests({url:`${BASE_URL}/films/3`, json:true}, async function (error, response, body) {
+    if(error){
+        console.log(error)
+    }
+    const { characters } = await response.body;
+    //    console.log(characters)
+       characters.forEach(element => {
+            new Promise( (resolve,reject)=>{
+            requests({uri:element,json:true}, async function(error, response){
+                // const {name} = await response.body
+                console.log(await response.body.name)
+            })
+            })
+       });
 
-request({ url: `${BASE_URL}/films/${argument}`, json: true }, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const characters = response.body.characters;
-    const promises = characters.map(character => {
-      return new Promise(resolve => {
-        request({ url: character, json: true }, function (error, response, body) {
-          if (error) {
-            console.log(error);
-          } else {
-            resolve(body.name);
-          }
-        });
-      });
-    });
-    Promise.all(promises)
-      .then(names => {
-        console.log(names.join('\n'));
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-});
+ });
