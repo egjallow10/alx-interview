528c4ad0c78c3392d3cbe8f817a377a510f19b1b
egjallow10 <egjallow10@gmail.com>
2023-05-07 17:21:17 -0700

    fix standand issues

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,29 +1,28 @@
 #!/usr/bin/node
 
-const requests = require('request')
-const BASE_URL = 'https://swapi-api.alx-tools.com/api'
-const argument = process.argv[2]
+const requests = require('request');
+const BASE_URL = 'https://swapi-api.alx-tools.com/api';
+const argument = process.argv[2];
 
 requests({ url: `${BASE_URL}/films/${argument}`, json: true }, async function (error, response, body) {
   if (error) {
-    console.log(error)
+    console.log(error);
   }
-  const characters = body.characters
+  const characters = body.characters;
   for (let index = 0; index < characters.length; index++) {
-    const name = await toGetName(characters[index])
-    console.log(name)
+    const name = await toGetName(characters[index]);
+    console.log(name);
   }
-})
+});
 
 function toGetName (url) {
   const name = new Promise((resolve, reject) => {
     requests({ url }, (error, response, body) => {
       if (error) {
-        console.log(error)
+        console.log(error);
       }
-      resolve(JSON.parse(body).name)
-    })
-  })
-  return name
+      resolve(JSON.parse(body).name);
+    });
+  });
+  return name;
 }
-
