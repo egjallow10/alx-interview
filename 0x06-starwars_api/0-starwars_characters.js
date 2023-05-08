a608a1efa0f6e8e387605b44fa6e3206501e1839
egjallow10 <egjallow10@gmail.com>
2023-05-07 17:09:26 -0700

    fix standand issues

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,31 +1,28 @@
 #!/usr/bin/node
 
-const requests = require('request');
-const { json } = require('stream/consumers');
-const BASE_URL = `https://swapi-api.alx-tools.com/api`
+const requests = require('request')
+const BASE_URL = 'https://swapi-api.alx-tools.com/api'
 const argument = process.argv[2]
 
-requests({url:`${BASE_URL}/films/3`, json:true}, async function (error, response, body) {
-    if(error){
+requests({ url: `${BASE_URL}/films/${argument}`, json: true }, async function (error, response, body) {
+  if (error) {
+    console.log(error)
+  }
+  const characters = body.characters
+  for (let index = 0; index < characters.length; index++) {
+    const name = await toGetName(characters[index])
+    console.log(name)
+  }
+})
+
+function toGetName (url) {
+  const name = new Promise((resolve, reject) => {
+    requests({ url }, (error, response, body) => {
+      if (error) {
         console.log(error)
-    }
-    const characters  = body["characters"]
-    for(let index = 0; index < characters.length; index++){
-        const name = await toGetName(characters[index])
-        console.log(name)
-    }
-       
-
- });
-
- function toGetName(url){
-    const name = new Promise((resolve, reject)=>{
-    requests({url: url},(error, response, body)=>{
-        if(error){
-            console.log(error)
-        }
-        resolve(JSON.parse(body).name)
-    });
-    });
-    return name
- }
+      }
+      resolve(JSON.parse(body).name)
+    })
+  })
+  return name
+}
